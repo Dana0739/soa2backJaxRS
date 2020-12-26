98509245741f7f3dfeff328e3f0a083b02e60842
Dana Zhetesova <zetesovadana@gmail.com>
2020-12-26 04:15:38 +0300

    bugfix + tested integratively with other services

--- src/main/java/com/dana/controller/HrServer.java
+++ src/main/java/com/dana/controller/HrServer.java
@@ -7,6 +7,7 @@ import com.dana.model.Status;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,23 +27,31 @@ public class HrServer {
                            @PathParam("position") String position, @PathParam("status") String status,
                            @PathParam("start-date") String date) {
         try {
-            System.out.println("kek1");
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             Date startDate = dateFormat.parse(date);
-            return HrClient.callXmlHrHireFireWorkerDTO(personId,
+            Response response = HrClient.callXmlHrHireFireWorkerDTO(personId,
                     new HRHireFireWorkerDTO(orgId, Position.getByTitle(position),
                             Status.getByTitle(status), startDate));
+            System.out.println(response.getStatus());
+            System.out.println(response.getEntity().toString());
+            System.out.println(response.getStatusInfo().toString());
+            return response;
         } catch (IllegalArgumentException e) {
             return Response.status(422).entity(e.getMessage()).build();
-        } catch (Exception e) {
+        } catch (ParseException e) {
             return Response.status(422).entity("Could not parse date format: yyyy-MM-dd").build();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            for (int i = 0; i < e.getStackTrace().length; i++) {
+                System.out.println(e.getStackTrace()[i].toString());
+            }
+            return Response.status(500).entity(e.getMessage()).build();
         }
     }
 
     @POST
     @Path("/fire/{id}")
     public Response hrHire(@PathParam("id") long id) {
-        System.out.println("kek2");
         return HrClient.callXmlHrHireFireWorkerDTO(id, new HRHireFireWorkerDTO());
     }
 }
